6c27b7b892240462982f99851124e20fdf6a9453
yeonsu <yeonsu826@gmail.com>
2026-07-21 23:45:21 +0900

    Feat: 영상 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -530,10 +530,14 @@ function Works() {
 function MyPortfolio() {
   // 1. 띄우고 싶은 인스타 영상 주소들을 쉼표(,)로 구분해서 넣으세요.
   const instagramUrls = [
-    "https://www.instagram.com/reel/DaaF1gPhC5X/",
     "https://www.instagram.com/reel/DX0-xahAll4/",
     "https://www.instagram.com/reel/DZHSKgSPtdv/",
     "https://www.instagram.com/reel/DV0-jS0k0JN/",
+    "https://www.instagram.com/reel/DUuHznCkt_-/",
+    "https://www.instagram.com/reel/DT39vTvku1d/",
+    "https://www.instagram.com/reel/DQ_ZbRYCUMg/",
+    "https://www.instagram.com/reel/DQ6c7jWEmkz/",
+    "https://www.instagram.com/reel/C-NS8YMyVdy/",
   ];
 
   return (
