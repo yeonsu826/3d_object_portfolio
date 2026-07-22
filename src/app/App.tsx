bab6d59afbeeeb1612eebfeb0965261240d081c6
yeonsu <yeonsu826@gmail.com>
2026-07-22 23:55:54 +0900

    Feat: text 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -579,10 +579,10 @@ function VideoGallery() {
     { id: "1211900004", type: "portrait", tools: ["Blender", "Unreal Engine", "Substance Painter"] }, 
     { id: "1211913186", type: "portrait", tools: ["Blender", "Unreal Engine", "Substance Painter"] }, 
 
-    { id: "1211907156", type: "landscape", tools: ["Unity", "C#"] },
-    { id: "1211907154", type: "landscape", tools: ["Unity", "C#"] },
-    { id: "1211907153", type: "landscape", tools: ["Unity", "C#"] },
-    { id: "1211907155", type: "landscape", tools: ["Unity", "C#"] },
+    { id: "1211907156", type: "landscape", tools: ["Unity", "C#", "Particle System"] },
+    { id: "1211907154", type: "landscape", tools: ["Unity", "C#", "Shader"] },
+    { id: "1211907153", type: "landscape", tools: ["Unity", "C#", "Particle Effect"] },
+    { id: "1211907155", type: "landscape", tools: ["Unity", "C#", "Particle Effect"] },
   ];
 
   if (vimeoVideos.length === 0) return null;
