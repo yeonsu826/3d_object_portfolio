f092f09af0ad7167cedd60a29cd76e165367bd52
yeonsu <yeonsu826@gmail.com>
2026-08-26 20:08:03 +0900

    Feat : 영상 업로드, 수정

--- src/app/data/videos.ts
+++ src/app/data/videos.ts
@@ -11,8 +11,9 @@ export const VIMEO_VIDEOS: VideoItem[] = [
   { id: "1211900047", type: "portrait", tools: ["Blender", "Unreal Engine", "Substance Painter"] },
   { id: "1211900004", type: "portrait", tools: ["Blender", "Unreal Engine", "Substance Painter"] },
   { id: "1211913186", type: "portrait", tools: ["Blender", "Unreal Engine", "Substance Painter"] },
+  { id: "1221433562", type: "landscape", tools: ["Unity", "UI Particle"] },
   { id: "1211907156", type: "landscape", tools: ["Unity", "C#", "Particle System"] },
   { id: "1211907154", type: "landscape", tools: ["Unity", "C#", "Shader"] },
-  { id: "1211907153", type: "landscape", tools: ["Unity", "C#", "Particle Effect"] },
-  { id: "1211907155", type: "landscape", tools: ["Unity", "C#", "Particle Effect"] },
+  { id: "1211907153", type: "landscape", tools: ["Unity", "C#", "UI Particle"] },
+  { id: "1211907155", type: "landscape", tools: ["Unity", "C#", "UI Particle"] },
 ];
